f26a9038be94cabf3c470b2cfb1dfe4e4bea3643
snowlinecn <snowlinecn@qq.com>
2018-02-01 10:09:33 +0800

    tkinter

--- p5.py
+++ p5.py
@@ -1,4 +1,15 @@
-import Tkinter
-top = Tkinter.Tk()
-# 进入消息循环
-top.mainloop()
+from tkinter import *
+master = Tk()
+Label(master, text="First").grid(row=0, sticky = W)
+Label(master, text="Second").grid(row=1, sticky = W)
+
+e1 = Entry(master)
+e2 = Entry(master)
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+button = Button(master,text = "button")
+button.grid(row = 0, column = 2, columnspan = 2, rowspan = 2, padx = 5,
+            pady = 5, sticky = W + E + N + S)
+mainloop()
