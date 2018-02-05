5b0c00686e73d4f1762f532ea3bc1ff385faa57a
snowlinecn <snowlinecn@qq.com>
2018-02-05 10:24:10 +0800

    test

--- p5.py
+++ p5.py
@@ -1,8 +1,9 @@
 from tkinter import *
 
 master = Tk()
-Label(master, text="First").grid(row=0, sticky = W)
-Label(master, text="Second").grid(row=1, sticky = W)
+master.title("Hello World!")
+Label(master, text="用户名", fg="red").grid(row=0, sticky = W)
+Label(master, text="密   码").grid(row=1, sticky = W)
 
 e1 = Entry(master)
 e2 = Entry(master)
@@ -11,6 +12,6 @@ e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
 
 button = Button(master,text = "button")
-button.grid(row = 0, column = 2, columnspan = 2, rowspan = 2, padx = 5,
-            pady = 5, sticky = W + E + N + S)
+button.grid(row = 0, column = 2, columnspan = 2, rowspan = 2, padx = 5, pady = 5, sticky = W + E + N + S)
+
 mainloop()
