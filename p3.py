c4c362e0b173a85d0b46bca93b16656914a94345
snowlinecn <snowlinecn@qq.com>
2018-04-17 19:23:23 +0800

    ss

--- p3.py
+++ p3.py
@@ -1,17 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-
-res = requests.get('http://www.qhu.edu.cn')
-res.encoding = 'utf-8'
-
-soup = BeautifulSoup(res.text, 'html.parser')
-
-soup = soup.select('.articleList_rt01a')
-
-
-for news in soup:
-    if len(news.select('.Date')) > 0:
-        h = news.select('li')[0].text
-        date = news.select('.Date')[0].text
-        a = news.select('a')[0]['href']
-        print(date, h, a)
+import os
+dir(os.path)
