26edce0bd625c391249e3106fbdc9ae18e4e829e
snowlinecn <snowlinecn@qq.com>
2018-05-10 14:57:29 +0800

    提交

--- p5.py
+++ p5.py
@@ -5,11 +5,8 @@ master.title("Hello World!")
 Label(master, text="用户名", fg="red").grid(row=0, sticky = W)
 Label(master, text="密   码").grid(row=1, sticky = W)
 
-e1 = Entry(master)
-e2 = Entry(master)
-
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
+e1 = Entry(master).grid(row=0, column=1)
+e2 = Entry(master).grid(row=1, column=1)
 
 button = Button(master,text = "button")
 button.grid(row = 0, column = 2, columnspan = 2, rowspan = 2, padx = 5, pady = 5, sticky = W + E + N + S)
