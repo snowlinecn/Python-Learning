5090ca321adf0c8a5f16d1cc593c37d7093983b6
snowlinecn <snowlinecn@qq.com>
2018-05-15 19:05:23 +0800

    改用类描述

--- p3.py
+++ p3.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+#from tkinter import ttk
 import tkinter.filedialog
 
 class MainWindow(Frame):
@@ -9,34 +9,36 @@ class MainWindow(Frame):
         self.createWidgets()
 
     def createWidgets(self):
-
-        #self.title("照片整理工具 V0.9")
-        #self.geometry("500x300")
-
-        self.frame = Frame(self, padding=10, relief="solid", borderwidth=2)
-        self.frame.grid(padx=10, pady=10)
-        '''
-        self.slb = Label(frame,text = '照片所在文件夹')
+        #self["padding"]=10
+        #, relief="solid", borderwidth=2)
+        self.grid(padx=10, pady=10)
+        
+        self.slb = Label(self,text = '照片所在文件夹')
         self.slb.grid(row=0,column=1,sticky=W)
 
-        self.dlb = Label(frame,text = '照片移动到文件夹')
+        self.dlb = Label(self,text = '照片移动到文件夹')
         self.dlb.grid(row=1,column=1,sticky=W)
 
-        self.sbtn = Button(frame,text="原照片文件夹",command=spwd)
+        self.sbtn = Button(self,text="原照片文件夹",command=self.spwd)
         self.sbtn.grid(row=0,column=0,sticky=W)
-        self.dbtn = Button(frame,text="移动到文件夹",command=dpwd)
+
+        self.dbtn = Button(self,text="移动到文件夹",command=self.dpwd)
         self.dbtn.grid(row=1,column=0,sticky=W)
+
+        self.txt = Text(self)
+        self.txt.grid(row=2,column=0,columnspan=2,sticky=W)
 
     def spwd(self):
         spath = tkinter.filedialog.askdirectory()
         if spath != '':
-            slb.config(text = spath)
+            self.slb.config(text = spath)
 
     def dpwd(self):
         dpath = tkinter.filedialog.askdirectory()
         if dpath != '':
-            dlb.config(text = dpath)
-'''      
+            self.dlb.config(text = dpath)
+  
 app = MainWindow()
-app.master.title("照片整理工具 V0.9")
+app.master.title('照片整理工具 V0.9')
+app.master.geometry('500x300')
 app.mainloop()
