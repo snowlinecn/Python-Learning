42eb018ed3ab42bd2667e1d5b467863fb2f4bb75
snowlinecn <snowlinecn@qq.com>
2018-05-22 14:59:19 +0800

    增加关东文本框

--- p3.py
+++ p3.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import scrolledtext
 import tkinter.filedialog
 
 class MainWindow(Frame):
@@ -13,37 +14,44 @@ class MainWindow(Frame):
         #, relief="solid", borderwidth=2)
         self.grid(padx=10, pady=10)
         
-        self.slb = Label(self,text = '照片所在文件夹')
+        self.slb = Label(self,text = '')
         self.slb.grid(row=0,column=1, sticky=W)
 
-        self.dlb = Label(self,text = '照片移动到文件夹')
+        self.dlb = Label(self,text = '')
         self.dlb.grid(row=1,column=1, sticky=W)
 
-        self.sbtn = Button(self,text="原照片文件夹",command=self.spwd)
+        self.sbtn = Button(self,text="请选择照片位置",command=self.spwd)
         self.sbtn.grid(row=0,column=0, sticky=W)
 
-        self.dbtn = Button(self,text="移动到文件夹",command=self.dpwd)
+        self.dbtn = Button(self,text="请选择移动到...",command=self.dpwd)
         self.dbtn.grid(row=1,column=0, sticky=W)
         
         self.mvtn = Button(self, text="移动", command=self.move_photo)
         self.mvtn.grid(row=2, column=0, sticky=W)
 
-        self.txt = Text(self)
-        self.txt.grid(row=3,column=0,columnspan=2, sticky=W)
+        self.scr = scrolledtext.ScrolledText(self, width=80, height=10) 
+        self.scr.grid(row=3, column=0, columnspan=2)
 
     def spwd(self):
         spath = tkinter.filedialog.askdirectory()
         if spath != '':
             self.slb.config(text = spath)
+            return spath
 
     def dpwd(self):
         dpath = tkinter.filedialog.askdirectory()
         if dpath != '':
             self.dlb.config(text = dpath)
+            return dpath
+   
     def move_photo(self):
-        pass
+        for i in range(0,1000):
+            self.scr.insert(END, str(i)+"\n")
+        self.scr.see(END)
+
+
     
 app = MainWindow()
 app.master.title('照片整理工具 V0.9')
-app.master.geometry('500x300')
+app.master.geometry('600x300')
 app.mainloop()
