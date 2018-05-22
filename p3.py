3d7e79ba0ad20f4fb6e4f7702c7f923397387d28
snowlinecn <snowlinecn@qq.com>
2018-05-22 20:54:01 +0800

    更改代码格式

--- p3.py
+++ p3.py
@@ -7,47 +7,49 @@ class MainWindow(Frame):
     
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self["padx"]=1
-        self["pady"]=1
+        self["padx"] = 1
+        self["pady"] = 1
+        self.spath = ''
+        self.dpath = ''
         self.createWidgets()
 
     def createWidgets(self):
 
         self.grid(padx=10, pady=10)
         
-        self.slb = Label(self,text = '')
-        self.slb.grid(row=0,column=1, sticky=W)
+        self.slb = Label(self, text=self.spath, justify="left")
+        self.slb.grid(row=0, column=1, sticky=W)
 
-        self.dlb = Label(self,text = '')
-        self.dlb.grid(row=1,column=1, sticky=W)
+        self.dlb = Label(self, text=self.dpath, justify="left")
+        self.dlb.grid(row=1, column=1, sticky=W)
 
-        self.sbtn = Button(self,text="选择照片位置",width=15,command=self.spwd)
-        self.sbtn.grid(row=0,column=0, sticky=W)
+        self.sbtn = Button(self, text="源文件夹", width=15, command=self.get_spwd)
+        self.sbtn.grid(row=0, column=0, sticky=W)
 
-        self.dbtn = Button(self,text="选择移动到...",width=15,command=self.dpwd)
-        self.dbtn.grid(row=1,column=0, sticky=W)
+        self.dbtn = Button(self, text="目标文件夹", width=15, command=self.get_dpwd)
+        self.dbtn.grid(row=1, column=0, sticky=W)
         
-        self.mvtn = Button(self, text="移动",width=15, command=self.move_photo)
+        self.mvtn = Button(self, text="开始移动", width=15, command=self.move_photo)
         self.mvtn.grid(row=2, column=0, sticky=W)
 
         self.scr = scrolledtext.ScrolledText(self, width=80, height=18) 
         self.scr.grid(row=3, column=0, columnspan=2)
 
-    def spwd(self):
-        spath = tkinter.filedialog.askdirectory()
-        if spath != '':
-            self.slb.config(text = spath)
-            return spath
+    def get_spwd(self):
+        self.spath = tkinter.filedialog.askdirectory()
+        if self.spath != '':
+            self.slb.config(text=self.spath)
+        else:
+            self.slb.config(text="请选择照片文件夹")
 
-    def dpwd(self):
-        dpath = tkinter.filedialog.askdirectory()
-        if dpath != '':
-            self.dlb.config(text = dpath)
-            return dpath
-   
+    def get_dpwd(self):
+        self.dpath = tkinter.filedialog.askdirectory()
+        if self.dpath != '':
+            self.dlb.config(text=self.dpath)
+        else:
+            self.dlb.config(text="请选择目标文件夹")
+            
     def move_photo(self):
-        # for i in range(0,1000):
-        #     self.scr.insert(END, str(i)+"\n")
         self.scr.insert(END, "照片将被移动到")
         self.scr.see(END)
     
