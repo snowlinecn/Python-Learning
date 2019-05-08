1278df068e16228a74c706d60beee056a1aeca9e
snowlinecn <snowlinecn@qq.com>
2019-05-08 12:59:25 +0800

    sub

--- zbgg_new.py
+++ zbgg_new.py
@@ -9,7 +9,7 @@ class ZbSpider(object):
     
     url = "http://www.ccgp-qinghai.gov.cn/es-articles/es-article/_search"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) \
-Gecko/20100101 Firefox/63.0", "Content-Type": "application/json"}
+Gecko/20100101 Firefox/63.0", "Content-Type": "application/json"} # 提交查询类型为application/json
     # Payload查询参数json
     payloadData = {
         "from": 0,
@@ -77,8 +77,8 @@ Gecko/20100101 Firefox/63.0", "Content-Type": "application/json"}
  
     # 获取中标公告列表   
     def zbgg_list(self): 
-        html = requests.post(self.url, data=json.dumps(self.payloadData), headers=self.headers)
-        d = json.loads(html.content.decode())
+        html_text = requests.post(self.url, data=json.dumps(self.payloadData), headers=self.headers)
+        d = json.loads(html_text.content.decode())
         size = self.payloadData['size']
         for i in range(size):
             self.zbgg_addrs.append("http://www.ccgp-qinghai.gov.cn"+d[ 'hits']['hits'][i]['_source']['url']) # 获取中标公告页面地址
@@ -87,21 +87,23 @@ Gecko/20100101 Firefox/63.0", "Content-Type": "application/json"}
 
     # 打印中标公告信息    
     def zbgg_print(self): 
+        i = 1
         for zbgg_adddr in self.zbgg_addrs:
             html_text = requests.get(zbgg_adddr, headers=self.headers, timeout=50).content.decode()
-            html_text = html.unescape(html_text)
-            pbzj = []
-            pbzj = re.findall(r'code-85005\\">(.*?)<', html_text) # 匹配评审专家名单
-            if pbzj == [] :
-                pbzj = re.findall(r'名单：(.*?)十', html_text) # 匹配不规范页面中的评审专家名单
-                pattern = re.compile(u"[\u4e00-\u9fa5、（）]") # 提取专家名单，保留汉字和"、（）"等符号，去除多余字符
-                pbzj[0] = "".join(pattern.findall(pbzj[0])) # 连接成专家名单字符串
+            html_text = html.unescape(html_text) # 反转义
+            #pbzj = []
+            #pbzj = re.findall(r'code-85005\\">(.*?)<', html_text) # 匹配评审专家名单
+            #if pbzj == [] :
+            pbzj = re.findall(r'名单：(.*?)十', html_text) # 匹配不规范页面中的评审专家名单
+            pattern = re.compile(u"[\u4e00-\u9fa5、（）]") # 提取专家名单，保留汉字和"、（）"等符号，去除多余字符
+            pbzj[0] = "".join(pattern.findall(pbzj[0])) # 连接成专家名单字符串
             cjrq = re.findall(r'code-94002\\">(.*?)<', html_text) # 匹配成交日期
             xmmc = re.findall(r'code-00003\\">(.*?)<', html_text) # 匹配项目名称
             xmbh = re.findall(r'code-00004\\">(.*?)<', html_text) # 匹配项目编号
-            print(cjrq[0], ",", xmbh[0], ",", xmmc[0], ",", pbzj[0])
+            print("["+str(i)+"] "+cjrq[0]+", "+xmbh[0]+", "+xmmc[0]+", "+pbzj[0])
+            i = i + 1
     
 if __name__ == "__main__":
-    zb = ZbSpider(0,50)
+    zb = ZbSpider(0,100)
     zb.zbgg_list()
     zb.zbgg_print()
